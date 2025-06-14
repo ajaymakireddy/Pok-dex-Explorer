393140b5ca5343b222782f22230c10f67d8b7471
AJAY <ajaymakireddy@gmail.com>
2025-06-14 08:01:45 +0530

    Fix: corrected case of App.css import

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,9 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 import { ThemeProvider } from './context/ThemeContext';
-import './app.css';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
